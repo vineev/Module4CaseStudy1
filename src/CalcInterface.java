5d16a163584d573455c4cbc8ca24aa74a4290f51
vineev <43990891+vineev@users.noreply.github.com>
2021-11-18 14:31:43 +0530

    Create CalcInterface.java
    initial commit by dev1

---
+++ src/CalcInterface.java
@@ -0,0 +1,12 @@
+
+public interface Calculator {
+
+	
+	public int Addition(int num1, int num2);
+	public int Subtraction(int num1, int num2);
+
+	public int Division(int num1, int num2);
+	public int Multiplicaton(int num1, int num2);
+
+		
+}
