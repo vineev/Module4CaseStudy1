347e37a178e376d4312e70588794cdfd32366523
vineela <vineelav9@gmail.com>
2021-11-18 14:33:42 +0530

    adding new file

---
+++ src/CalculatorImpl.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CalculatorImpl implements Calculator {
+
+	@Override
+	public int Addition(int num1, int num2) {
+		// TODO Auto-generated method stub
+
+		return num1 + num2;
+
+	}
+
+	@Override
+	public int Subtraction(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Division(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Multiplicaton(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
